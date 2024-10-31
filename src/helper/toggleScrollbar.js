7440cd5835e49cbd3857713045281c908e16f6b7
iam-SayanDas <sayandas.devs@gmail.com>
2024-11-01 00:14:57 +0530

    init: initial commit

---
+++ src/helper/toggleScrollbar.js
@@ -0,0 +1,43 @@
+export function toggleScrollbar(isOpen) {
+  const getScrollbarWidth = () => {
+    const div = document.createElement("div");
+    div.style.overflow = "scroll";
+    div.style.width = "100px";
+    div.style.height = "100px";
+    div.style.position = "absolute";
+    document.body.appendChild(div);
+
+    const scrollbarWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+    return scrollbarWidth;
+  };
+
+  const body = document.body;
+  if (isOpen) {
+    const scrollbarWidth = getScrollbarWidth();
+    body.style.paddingRight = `${scrollbarWidth}px`;
+    body.classList.add("overflow-y-hidden");
+
+    const style = document.createElement("style");
+    style.id = "hide-scrollbar";
+    style.innerHTML = `::-webkit-scrollbar {display:none;}`;
+    document.head.appendChild(style);
+  } else {
+    body.style.paddingRight = "0";
+    body.classList.remove("overflow-y-hidden");
+    const styleElement = document.getElementById("hide-scrollbar");
+    if (styleElement) {
+      styleElement.remove();
+    }
+  }
+}
+
+export function cleanupScrollbar() {
+  const body = document.body;
+  body.style.paddingRight = "0"; // Cleanup padding
+  body.classList.remove("overflow-y-hidden");
+  const styleElement = document.getElementById("hide-scrollbar");
+  if (styleElement) {
+    styleElement.remove();
+  }
+}
